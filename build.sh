d7efcfa5c090c8b1a23486e2b2badd9d4308a171
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-07-07 20:55:38 +0300

    ci_3

--- build.sh
+++ build.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 set -e
 
-mkdir -p /workspace/build
-cd /workspace/build
+cd /build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j$(nproc)
 
 # Копирование артефактов с абсолютными путями
-cp /workspace/build/server/telemetry_server /workspace/bin/
-cp /workspace/build/client/telemetry_client /workspace/bin/
-cp /workspace/build/tests/unit_tests/unit_tests /workspace/bin/
-cp /workspace/build/tests/functional_tests/functional_tests /workspace/bin/
+cp /build/server/telemetry_server /bin/
+cp /build/client/telemetry_client /bin/
+cp /build/tests/unit_tests/unit_tests /bin/
+cp /build/tests/functional_tests/functional_tests /bin/
 
 # Копирование тестовых данных
-mkdir -p /workspace/bin/test_data
-cp -r /workspace/tests/functional_tests/test_data/* /workspace/bin/test_data/
-cp -r /workspace/tests/functional_tests/test_scripts/* /workspace/bin/test_data/
+mkdir -p /bin/test_data
+cp -r /tests/functional_tests/test_data/* /bin/test_data/
+cp -r /tests/functional_tests/test_scripts/* /bin/test_data/
 
 # Устанавливаем права на ВСЕ файлы
-chmod -R +x /workspace/bin/*
+chmod -R +x /bin/*
 
 echo "Build completed successfully!"
