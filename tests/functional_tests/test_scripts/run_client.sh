fb5fac7127d7bd48ba1d6fbc1dc20778e8abd311
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-07-08 12:06:17 +0300

    CI_3

--- tests/functional_tests/test_scripts/run_client.sh
+++ tests/functional_tests/test_scripts/run_client.sh
@@ -4,6 +4,7 @@ TEST_NAME=$1
 export LC_ALL=C
 INPUT_FILE="/test_data/${TEST_NAME}_input.txt"
 OUTPUT_FILE="/test_data/${TEST_NAME}_output.txt"
+touch "$OUTPUT_FILE"
 
 /app/telemetry_client telemetry_server 12345 < "$INPUT_FILE" > "$OUTPUT_FILE" 2>&1
 EXIT_CODE=$?
