d517b25a4c0c6c045909ffe56fd53dbca44a7ba2
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-07-25 13:16:15 +0300

    rm signal_handler

--- server/main.cpp
+++ server/main.cpp
@@ -3,15 +3,10 @@
 #include <iostream>
 #include <csignal>
 
-std::function<void()> shutdown_handler;
-void signal_handler(int) {
-    if (shutdown_handler) shutdown_handler();
-}
-
 int main(int argc, char* argv[]) {
     try {
         if (argc != 3) {
-            std::cerr << "Usage: " << argv[0] << " <port> <config_path>\n";
+            std::cout << "Usage: " << argv[0] << " <port> <config_path>\n";
             return 1;
         }
 
@@ -24,7 +19,6 @@ int main(int argc, char* argv[]) {
         
         server->start();
         
-        // Обработка сигналов
         boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
         signals.async_wait([&](const auto&, int) {
             std::cout << "Stopping server..." << std::endl;
@@ -32,7 +26,6 @@ int main(int argc, char* argv[]) {
             io_context.stop();
         });
         
-        // Запускаем IO контекст в нескольких потоках
         const size_t num_threads = std::max(1u, std::thread::hardware_concurrency() / 2);
         std::vector<std::thread> threads;
         for(size_t i = 0; i < num_threads; ++i) {
@@ -48,7 +41,7 @@ int main(int argc, char* argv[]) {
         }
         
     } catch (const std::exception& e) {
-        std::cerr << "Server exception: " << e.what() << std::endl;
+        std::cout << "Server exception: " << e.what() << std::endl;
         return 1;
     }
     return 0;
